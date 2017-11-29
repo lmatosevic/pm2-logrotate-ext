40bacb20f9007b257a34b336e4a63b9900565218
vmarchaud <contact@vmarchaud.fr>
2017-11-29 16:24:55 +0100

    wip

--- app.js
+++ app.js
@@ -28,23 +28,6 @@ var conf = pmx.initModule({
 var PM2_ROOT_PATH = '';
 var Probe = pmx.probe();
 
-if (process.env.PM2_HOME)
-  PM2_ROOT_PATH = process.env.PM2_HOME;
-else if (process.env.HOME && !process.env.HOMEPATH)
-  PM2_ROOT_PATH = path.resolve(process.env.HOME, '.pm2');
-else if (process.env.HOME || process.env.HOMEPATH)
-  PM2_ROOT_PATH = path.resolve(process.env.HOMEDRIVE, process.env.HOME || process.env.HOMEPATH, '.pm2');
-
-var WORKER_INTERVAL = isNaN(parseInt(conf.workerInterval)) ? 30 * 1000 : 
-                            parseInt(conf.workerInterval) * 1000; // default: 30 secs
-var SIZE_LIMIT = get_limit_size(); // default : 10MB
-var ROTATE_CRON = conf.rotateInterval || "0 0 * * *"; // default : every day at midnight
-var RETAIN = isNaN(parseInt(conf.retain)) ? undefined : parseInt(conf.retain); // All
-var COMPRESSION = JSON.parse(conf.compress) || false; // Do not compress by default
-var DATE_FORMAT = conf.dateFormat || 'YYYY-MM-DD_HH-mm-ss';
-var ROTATE_MODULE = JSON.parse(conf.rotateModule) || true;
-var WATCHED_FILES = [];
-
 function get_limit_size() {
   if (conf.max_size == '')
     return (1024 * 1024 * 10);
